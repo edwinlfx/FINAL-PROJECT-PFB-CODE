4d2bc09aeff7df6006c12ab27a8894f227e5a75f
edwinlfx <winwinwinniethepooh@gmail.com>
2023-01-27 08:37:32 +0800

    reading overheads csv file

--- overheads.py
+++ overheads.py
@@ -1,1 +1,8 @@
 #import Path from pathliub and csv module
+from pathlib import Path
+import csv
+
+fp =Path.cwd()/"csv_reports"/"overheads-day-90.csv"
+with fp.open(mode="r", encoding="UTF-8", newline ="") as file:
+    reader = csv.reader(file)
+    next(reader)
