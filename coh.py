b5c0c4053298d703b9a066ca3c4200c051b01062
edwinlfx <winwinwinniethepooh@gmail.com>
2023-01-28 09:19:53 +0800

    append data extracted from csv to data variable

--- coh.py
+++ coh.py
@@ -5,3 +5,5 @@ with fp.open(mode="r", encoding="UTF-8", newline="") as file:
     reader = csv.reader(file)
     next(reader)
     data = []
+    for column in reader:
+        data.append([column[0]], [column[1]])
