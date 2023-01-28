b38a306e3486e2a85b7650f741536cd951ff5af4
edwinlfx <winwinwinniethepooh@gmail.com>
2023-01-28 08:00:46 +0800

    code

--- overheads.py
+++ overheads.py
@@ -6,3 +6,4 @@ fp =Path.cwd()/"csv_reports"/"overheads-day-90.csv"
 with fp.open(mode="r", encoding="UTF-8", newline ="") as file:
     reader = csv.reader(file)
     next(reader)
+    overheads = []
