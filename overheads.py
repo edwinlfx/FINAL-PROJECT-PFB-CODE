60999e4aeef1e3ee0f285b9090082d932a5b78a3
edwinlfx <winwinwinniethepooh@gmail.com>
2023-01-28 08:04:12 +0800

    for loop to append overhead category & % to list

--- overheads.py
+++ overheads.py
@@ -7,3 +7,5 @@ with fp.open(mode="r", encoding="UTF-8", newline ="") as file:
     reader = csv.reader(file)
     next(reader)
     overheads = []
+    for column in reader:
+        overheads.append([column[0], column[1]])
