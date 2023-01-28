15bf1970641908bdca39493a322c3337683af40d
edwinlfx <winwinwinniethepooh@gmail.com>
2023-01-28 08:32:10 +0800

    correction of path and reading csv

--- coh.py
+++ coh.py
@@ -1,5 +1,6 @@
-from pathlib import path
+from pathlib import Path
 import csv
 fp = Path.cwd()/"csv_reports"/"cash-on-hand-usd.csv"
 with fp.open(mode="r", encoding="UTF-8", newline="") as file:
-    
+    reader = csv.reader(file)
+    next(reader)
