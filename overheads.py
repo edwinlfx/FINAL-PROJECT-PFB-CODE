32eef15f6dfcd67754032eadb1348f1ea0a65b00
edwinlfx <winwinwinniethepooh@gmail.com>
2023-01-28 08:16:49 +0800

    overhead function

--- overheads.py
+++ overheads.py
@@ -1,7 +1,7 @@
 #import Path from pathliub and csv module
 from pathlib import Path
 import csv
-
+def overhead_function():
 fp =Path.cwd()/"csv_reports"/"overheads-day-90.csv"
 with fp.open(mode="r", encoding="UTF-8", newline ="") as file:
     reader = csv.reader(file)
