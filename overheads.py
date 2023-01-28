0755af0160316a26aeb92c3a2d4989a027b57087
edwinlfx <winwinwinniethepooh@gmail.com>
2023-01-28 08:05:27 +0800

    set highest overhead variable

--- overheads.py
+++ overheads.py
@@ -9,3 +9,5 @@ with fp.open(mode="r", encoding="UTF-8", newline ="") as file:
     overheads = []
     for column in reader:
         overheads.append([column[0], column[1]])
+
+highest_overhead = ["", 0]
