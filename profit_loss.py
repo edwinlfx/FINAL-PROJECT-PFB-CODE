508a9ee58f47b749dc742948b908351b2df9e970
generalkaiye <123610588+generalkaiye@users.noreply.github.com>
2023-01-30 11:50:38 +0800

    Update profit_loss.py

--- profit_loss.py
+++ profit_loss.py
@@ -4,4 +4,4 @@ import csv
 #define a profitloss_function() because function will be used in main.py
 def profitloss_function():
     #create a file path to the csv file 
-    
+    fp = Path.cwd()/"IGP CSV"/"profit-and-loss-usd.csv"
