dcdeb0c48969d9aae51b560aa810e483e72c55e9
generalkaiye <123610588+generalkaiye@users.noreply.github.com>
2023-01-30 11:47:31 +0800

    Update profit_loss.py

---
+++ profit_loss.py
@@ -0,0 +1,7 @@
+#import Path from pathlib and csv module 
+from pathlib import Path
+import csv
+#define a profitloss_function() because function will be used in main.py
+def profitloss_function():
+    #create a file path to the csv file 
+    
