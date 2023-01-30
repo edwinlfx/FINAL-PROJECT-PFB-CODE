0f54ba6f3848f2262547e9e2847b1d07c91f948d
Maximususs <123786733+Maximususs@users.noreply.github.com>
2023-01-30 12:11:05 +0800

    Update coh.py

--- coh.py
+++ coh.py
@@ -3,7 +3,10 @@ from pathlib import Path
 import csv
 #define a cash-on-hand (coh) function() which will be used in main.py 
 def coh_function():
+    #create a file path to the csv file
     fp = Path.cwd()/"csv_reports"/"cash-on-hand-usd.csv"
+    #read csv file using with syntax and .open() to append day number and deficit
+    #set mode to "r" in order to read the csv file
     with fp.open(mode="r", encoding="UTF-8", newline="") as file:
         reader = csv.reader(file)
         next(reader)
