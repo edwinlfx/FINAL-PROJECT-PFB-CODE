d978aa8b703d7193013ea094420d80914e5f8ae6
generalkaiye <123610588+generalkaiye@users.noreply.github.com>
2023-01-30 12:14:52 +0800

    Update profit_loss.py

--- profit_loss.py
+++ profit_loss.py
@@ -4,7 +4,7 @@ import csv
 #define a profitloss_function() because function will be used in main.py
 def profitloss_function():
     #create a file path to the csv file 
-    fp = Path.cwd()/"IGP CSV"/"profit-and-loss-usd.csv"
+    fp = Path.cwd()/"csv_report"/"profit-and-loss-usd.csv"
     #read csv file using with syntax and .open() to append day number and deficit
     #set mode to "r" in order to read csv file
     with fp.open(mode="r", encoding="UTF-8", newline = "") as file:
@@ -17,5 +17,6 @@ def profitloss_function():
         
         for column in reader:
             data.append([column[0], column[4]])
+            
             
             
