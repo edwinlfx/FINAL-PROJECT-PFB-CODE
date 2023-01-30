17a5eb3941ed8fb72274ebe11150e03d5f491f47
chlxeee <122858855+chlxeee@users.noreply.github.com>
2023-01-30 12:18:22 +0800

    Update overheads.py

--- overheads.py
+++ overheads.py
@@ -23,6 +23,7 @@ def overhead_function():
     #create "highest_overhead" variable as a list variable with an empty string and 0 in it
     #the highest overhead category and percentage from the overheads list will be apprended into highest overhead list
     highest_overhead = ["", 0]
+    #use for loop to iterate
     for item in overheads:
         if float(item[1]) > float(highest_overhead[1]):
             highest_overhead = item
